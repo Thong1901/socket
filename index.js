61c2149815c14330f0bf621c013ccefd60362b21
Thong1901 <thongthanhmai129@gmail.com>
2024-11-28 23:57:22 +0700

    t

--- index.js
+++ index.js
@@ -27,7 +27,54 @@ io.on("connection", (socket) => {
                 date: new Date(),
             });
         }
-    })
+    });
+    // *** Signaling cho gọi video ***
+    socket.on("sendOffer", (data) => {
+        const { recipientId, offer } = data;
+        const user = onlineUsers.find((user) => user.userId === recipientId);
+        if (user) {
+            io.to(user.socketId).emit("receiveOffer", { senderId: data.senderId, offer });
+        }
+    });
+    socket.on("sendAnswer", (data) => {
+        const { recipientId, answer } = data;
+        const user = onlineUsers.find((user) => user.userId === recipientId);
+        if (user) {
+            io.to(user.socketId).emit("receiveAnswer", { senderId: data.senderId, answer });
+        }
+    });
+    socket.on("sendIceCandidate", (data) => {
+        const { recipientId, candidate } = data;
+        const user = onlineUsers.find((user) => user.userId === recipientId);
+        if (user) {
+            io.to(user.socketId).emit("receiveIceCandidate", { senderId: data.senderId, candidate });
+        }
+    });
+    // Khi một người dùng từ chối cuộc gọi
+    socket.on("rejectCall", (data) => {
+        const { recipientId } = data;
+        const user = onlineUsers.find((user) => user.userId === recipientId);
+        if (user) {
+            io.to(user.socketId).emit("callRejected", { senderId: data.senderId });
+        }
+    });
+    // Khi một người dùng kết thúc cuộc gọi
+    socket.on("endCall", (data) => {
+        const { recipientId } = data;
+        const user = onlineUsers.find((user) => user.userId === recipientId);
+        if (user) {
+            io.to(user.socketId).emit("callEnded", { senderId: data.senderId });
+        }
+    });
+    socket.on("updateCallStatus", (data) => {
+        const { userId, status } = data;
+        const user = onlineUsers.find((user) => user.userId === userId);
+        if (user) {
+            user.status = status;
+            io.emit("callStatusUpdated", onlineUsers);
+        }
+    });
+
     socket.on("disconnect", () => {
         onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id)
         io.emit("getOnlineUsers", onlineUsers);
